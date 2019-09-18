ff9c553b0f7af9c9662cc0c602795ca011befdf8
hostogio <50251478+hostogio@users.noreply.github.com>
2019-09-17 21:33:22 -0300

    FreeWay2.0

---
+++ freeway/carros.js
@@ -0,0 +1,26 @@
+//carro
+let xCarros = [600, 600, 600,600, 600, 600];
+let yCarros = [40, 98, 150, 210, 260, 310];
+let velocidadesCarros = [9, 5, 2, 4, 8, 5];
+let comprimentoCarros = [50, 50, 80, 50, 50, 80];
+let altura = 40;
+
+
+
+function mostraCarro(){
+  //print("Quantidade de carros: "+imagemCarros.lenght);
+ for(let i=0; i< imagensDoCarros.length; i++){
+  image(imagensDoCarros[i], xCarros[i], yCarros[i], comprimentoCarros[i], altura);
+
+ }
+}
+  function movimentaCarro(){
+    for(let i=0; i < imagensDoCarros.length; i++){
+    xCarros[i] -= velocidadesCarros[i];   
+    if(xCarros[i] < -50){
+      xCarros[i] = 600; //volta á posiçao inicial
+    }  
+   }
+  }
+    
+
